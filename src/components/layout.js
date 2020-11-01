501872b8bcb43d5aa65accf3e31315acfc45be7f
Joey Robinson <theamazingness@gmail.com>
2020-11-01 11:50:41 -0500

    layout & styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,11 @@
 import React from "react"
-import Header from "./header"
 
 const Layout = ({ children, className }) => {
   return (
     <>
-      <Header />
       <main>
         <section className={className}>{children}</section>
       </main>
-      <footer
-        style={{
-          marginTop: `2rem`,
-        }}
-      >
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
 import React from "react"
+import ContactLeft from "../components/contact/contact.left"
+import ContactRight from "../components/contact/contact.right"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout className="home">
     <SEO title="Home" />
-    <div>onwegonwegwgon</div>
+    <div>
+      <hgroup>
+        <h1>Joey Robinson</h1>
+        <h2>Trainer Resume</h2>
+      </hgroup>
+      <div className="line" />
+      <div className="contact">
+        <div className="contact--left">
+          <ContactLeft />
+        </div>
+        <div className="contact--right">
+          <ContactRight />
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
